e99f2669b8cb519f00eadd776dfb0c8ca78c9244
NlightN22 <oncharterliz@gmail.com>
2025-11-04 13:12:46 +0700

    up

--- go/internal/version/version.go
+++ go/internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 // current holds the application version. It is overridden at build time via
 // -ldflags "-X github.com/NlightN22/xray-p2p/go/internal/version.current=...".
-var current = "0.1.0"
+var current = "0.1.1"
 
 // Current returns the application version string embedded at build time.
 func Current() string {
