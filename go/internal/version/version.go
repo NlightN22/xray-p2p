91869d2b0d80c15f1586068a522d72ae40ee7ec2
NlightN22 <oncharterliz@gmail.com>
2026-01-02 16:49:58 +0700

    chore: release v0.1.9

--- go/internal/version/version.go
+++ go/internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 // current holds the application version. It is overridden at build time via
 // -ldflags "-X github.com/NlightN22/xray-p2p/go/internal/version.current=...".
-var current = "0.1.8"
+var current = "0.1.9"
 
 // Current returns the application version string embedded at build time.
 func Current() string {
