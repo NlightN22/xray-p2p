4d4c8356a5d96b0510ede7f1cfa7e677f90b2558
NlightN22 <oncharterliz@gmail.com>
2026-01-02 15:12:39 +0700

    fix allow insecure in client install

--- go/internal/cli/client/install_command.go
+++ go/internal/cli/client/install_command.go
@@ -101,10 +101,7 @@ func runClientInstall(ctx context.Context, cfg config.Config, args []string) int
 	userValue := cfg.Client.User
 	passwordValue := cfg.Client.Password
 	serverNameValue := cfg.Client.ServerName
-	allowInsecureValue := true
-	if cfg.Client.AllowInsecure {
-		allowInsecureValue = true
-	}
+	allowInsecureValue := cfg.Client.AllowInsecure
 
 	if sniFlagProvided && !*strictTLS {
 		allowInsecureValue = true
